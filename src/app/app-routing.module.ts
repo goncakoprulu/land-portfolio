0f35506ef8016b30e04668f8a85398a38e138839
Gonca Köprülü <gonca.koprulu@reateknoloji.com>
2024-04-30 22:55:27 +0300

    Update tslib to version 2.6.2
    The package-lock.json and package.json files have been updated to use tslib version 2.6.2. The node\_modules/tslib and node

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DesignProjectsComponent } from './design-projects/design-projects.component';
 import { SoftwareProjectsComponent } from './components/software-projects/software-projects.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'design-projects', component: DesignProjectsComponent },
-  { path: 'software-projects', component: SoftwareProjectsComponent }
+  { path: 'software-projects', component: SoftwareProjectsComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
